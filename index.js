ff5aa86c0bc3e68862ea628020061dd3d3f0991b
Peter <urbpeti@gmail.com>
2018-06-01 13:38:01 +0200

    argument validation

--- index.js
+++ index.js
@@ -2,8 +2,48 @@ const {
   MILLISECOUNDS_IN_ONE_HOUR,
 } = require('./const/constants.js');
 
-function calculateDueDate(submitdate, turnaroundtime) {
-  return addHoursToDate(submitdate, turnaroundtime);
+const {
+  MISSING_ARGUMENTS,
+  INVALID_SUBMITTINGDATE,
+  INVALID_TURNAROUNDTIME,
+  SUBMITTING_MUST_BE_IN_WORKING_OURS,
+} = require('./messages/error/index.js');
+
+function calculateDueDate(submitDate, turnaroundTime) {
+  checkArguments(submitDate, turnaroundTime);
+  checkSubmitDateInWorkingHours(submitDate);
+
+  return addHoursToDate(submitDate, turnaroundTime);
+}
+
+function checkArguments(submitDate, turnaroundTime) {
+  if (submitDate === undefined || turnaroundTime === undefined) {
+    throw new Error(MISSING_ARGUMENTS);
+  }
+  if (!(submitDate instanceof Date)) {
+    throw new Error(INVALID_SUBMITTINGDATE);
+  }
+  if (!(Number.isInteger(turnaroundTime)) || turnaroundTime < 0) {
+    throw new Error(INVALID_TURNAROUNDTIME);
+  }
+}
+
+function checkSubmitDateInWorkingHours(submitDate) {
+  if (!inWorkingHours(submitDate) || isWeekend(submitDate)) {
+    throw new Error(SUBMITTING_MUST_BE_IN_WORKING_OURS);
+  }
+}
+
+function isWeekend(date) {
+  return date.getDay() === 6 || date.getDay() === 0;
+}
+
+function inWorkingHours(date) {
+  const dateTime = date.getTime();
+  const workStart = new Date(date).setHours(9, 0, 0, 0);
+  const workEnd = new Date(date).setHours(17, 0, 0, 0);
+
+  return workStart <= dateTime && dateTime < workEnd;
 }
 
 function addHoursToDate(date, hours) {
--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,75 @@
 const calculateDueDate = require('../');
+const {
+  MISSING_ARGUMENTS,
+  INVALID_SUBMITTINGDATE,
+  INVALID_TURNAROUNDTIME,
+  SUBMITTING_MUST_BE_IN_WORKING_OURS,
+} = require('../messages/error/index.js');
 
-describe('calculated due date', () => {
-  it('ends in the same day', () => {
-    const submitDate = new Date(2018, 6, 1, 8, 0, 0, 0);
-    const dueDate = calculateDueDate(submitDate, 3).getTime();
-    const expectedDate = new Date(2018, 6, 1, 11, 0, 0).getTime();
+
+describe('calculate due date', () => {
+  let submittingDate;
+
+  beforeEach(() => {
+    submittingDate = new Date(2018, 5, 1, 9, 0, 0, 0);
+  });
+
+  describe('with invalid arguments', () => {
+    test('invalid first argument', () => {
+      expect(() => calculateDueDate('not a date', 1))
+        .toThrow(INVALID_SUBMITTINGDATE);
+      expect(() => calculateDueDate(1, 1))
+        .toThrow(INVALID_SUBMITTINGDATE);
+    });
+
+    describe('submitting date is not in working ours', () => {
+      test('submitting date is before working ours', () => {
+        submittingDate = new Date(2018, 5, 1, 7, 0, 0, 0);
+        expect(() => calculateDueDate(submittingDate, 1))
+          .toThrow(SUBMITTING_MUST_BE_IN_WORKING_OURS);
+      });
+
+      test('submitting date is after working ours', () => {
+        submittingDate = new Date(2018, 5, 1, 17, 0, 0, 1);
+        expect(() => calculateDueDate(submittingDate, 1))
+          .toThrow(SUBMITTING_MUST_BE_IN_WORKING_OURS);
+      });
+
+      test('submitting date is in weekends', () => {
+        submittingDate = new Date(2018, 5, 2, 9, 0, 0, 0);
+        expect(() => calculateDueDate(submittingDate, 1))
+          .toThrow(SUBMITTING_MUST_BE_IN_WORKING_OURS);
+      });
+    });
+
+    test('invalid second argument', () => {
+      expect(() => calculateDueDate(submittingDate, 'not an integer'))
+        .toThrow(INVALID_TURNAROUNDTIME);
+      expect(() => calculateDueDate(submittingDate, 1.1))
+        .toThrow(INVALID_TURNAROUNDTIME);
+      expect(() => calculateDueDate(submittingDate, -1))
+        .toThrow(INVALID_TURNAROUNDTIME);
+    });
+
+    test('missing arguments', () => {
+      expect(() => calculateDueDate())
+        .toThrow(MISSING_ARGUMENTS);
+      expect(() => calculateDueDate(new Date()))
+        .toThrow(MISSING_ARGUMENTS);
+    });
+  });
+
+  test('due date ends in the same day', () => {
+    const dueDate = calculateDueDate(submittingDate, 3).getTime();
+    const expectedDate = new Date(2018, 5, 1, 12, 0, 0).getTime();
+
+    expect(dueDate).toBe(expectedDate);
+  });
+
+  xtest('due date ends in the next day', () => {
+    submittingDate = new Date(2018, 4, 31, 9, 0, 0, 0);
+    const dueDate = calculateDueDate(submittingDate, 8).getTime();
+    const expectedDate = new Date(2018, 5, 1, 9, 0, 0).getTime();
 
     expect(dueDate).toBe(expectedDate);
   });
